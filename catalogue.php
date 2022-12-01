6b2c35648cf2cc2e2fd93f655121042769e7913e
c2030860 <c2030860@hallam.shu.ac.uk>
2022-12-01 17:03:34 +0000

    Added Footer

--- catalogue.php
+++ catalogue.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -11,31 +12,41 @@
       href="css/desktop.css"
       media="only screen and (min-width : 720px)"
     />
+
   </head>
-  <body>
-<?php
-include("includes/header.php");
-?>
-    <div class="mainContainer">
-		      <main>
-              <div class="banner">
-                <h2>Catalogue</h2>
-              </div>
-              <section class="twoColumn">
-                <div>
-                <!-- Film Listing Here -->
-                </div>
-		            <div class="sideBar">
-                <h3>Featured Film</h3>
-                <div> <img src="images/babadook.jpg" alt="Babadook"/> </div>
-                <p>Info Here</p>
-                </div>
-              </section>
-        </main>
-    </div>
-<?php
-// add Footer
-?>
-    <script src="js/main.js"></script>
+<body>
+
+  <?php
+  include("includes/header.php");
+  ?>
+
+  <div class="mainContainer">
+    <main>
+        <div class="banner">
+          <h2>Catalogue</h2>
+        </div>
+
+        <section class="twoColumn">
+          <div>
+          <!-- Film Listing Here -->
+          </div>
+
+          <div class="sideBar">
+            <h3>Featured Film</h3>
+            <div> <img src="images/babadook.jpg" alt="Babadook"/> </div>
+            <p>Info Here</p>
+          </div>
+
+        </section>
+    </main>
+  </div>
+
+  <?php
+  // add Footer
+  include("includes/footer.php");
+  ?>
+
+  <script src="js/main.js"></script>
+
   </body>
 </html>
