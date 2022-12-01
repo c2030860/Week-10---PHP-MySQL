dff44c8cc7070a6d60028c88a3c9b41102710bd3
c2030860 <c2030860@hallam.shu.ac.uk>
2022-12-01 16:34:13 +0000

    Added Whitespace

--- index.php
+++ index.php
@@ -1,26 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
     <title>Welcome to SHU Films</title>
+
     <link rel="stylesheet" href="css/mobile.css" />
     <link
       rel="stylesheet"
       href="css/desktop.css"
       media="only screen and (min-width : 720px)"
     />
+
   </head>
   <body>
+
 <?php
 include("includes/header.php");
 ?>
+
     <div class="mainContainer">
 		<main>
+
       <div class="banner">
         <h2>Banner</h2>
       </div>
+      
       <section class="homePage">
 		<!-- Featured Films Here -->
       </section>
--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 (function () {
+
   let setMenu = function () {
     // variables
     let burgerMenu = document.querySelector(".burger");
     let navBar = document.querySelector("nav");
     let navBarStatus = false;
+
     if (navBar.style.left === "") {
       burgerMenu.addEventListener("click", () => {
         console.info(burgerMenu.classList);
+
         if (navBarStatus) {
           navBarStatus = false;
           navBar.removeAttribute("style");
           // burger animation class
           //burgerMenu.classList.toggle("changeBurger");
 		  burgerMenu.setAttribute("class", "burger");
-        } else {
+        } 
+        else 
+        {
           navBarStatus = true;
           navBar.style.left = "0px";
           // burger animation class
@@ -21,12 +26,14 @@
           burgerMenu.setAttribute("class", "burger changeBurger");
         }
       });
-    } else {
+    } 
+    else {
       navBarStatus = false;
       navBar.removeAttribute("style");
     }
   };
   setMenu();
+
   // resize
   window.addEventListener("resize", () => setMenu());
 })();
