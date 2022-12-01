dad9aa8e7938bc7e007f799c0587cc35bfcf79de
c2030860 <c2030860@hallam.shu.ac.uk>
2022-12-01 17:41:42 +0000

    Added POST Methods

--- contact.php
+++ contact.php
@@ -1,69 +1,88 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>Contact Us</title>
-<link rel="stylesheet" href="css/mobile.css" />
-<link
-      rel="stylesheet"
-      href="css/desktop.css"
-      media="only screen and (min-width : 720px)"
-    />
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Contact Us</title>
+
+  <link rel="stylesheet" href="css/mobile.css" />
+  <link
+        rel="stylesheet"
+        href="css/desktop.css"
+        media="only screen and (min-width : 720px)"
+      />
+
 </head>
+
 <body>
-<?php
-include("includes/header.php");
-?>
-<div class="mainContainer">
-  <main>
-    <div class="banner">
-      <h2>Contact Us</h2>
-    </div>
-    <section class="twoColumn">
-      <div>
-        <form action="thankyou.php" method="post">
-          <div>
-            <label for="firstname">First name</label>
-            <input type="text" name="firstname" id="firstname" placeholder="Your First Name"/>
-          </div>
-          <div>
-            <label for="surname">Surname</label>
-            <input type="text" name="surname" id="surname" placeholder="Your Surname"/>
-          </div>
-          <div>
-            <label for="Email">Email</label>
-            <input type="email" name="Email" id="Email" required />
-          </div>
-          <div>
-            <label for="Tel">Contact Telephone</label>
-            <input type="tel" name="Tel" id="Tel" required />
-          </div>
-          <div>
-            <label for="marketing">How did you hear about us?</label>
-            <select name="marketing" id="marketing">
-              <option value="Web">Web</option>
-              <option value="Friend">Friend</option>
-              <option value="Other">Other</option>
-            </select>
-          </div>
-          <div>
-            <input type="submit" value="Send" class="sendButton" />
-          </div>
-        </form>
+  <?php
+    include("includes/header.php");
+  ?>
+
+  <div class="mainContainer">
+    <main>
+      <div class="banner">
+        <h2>Contact Us</h2>
       </div>
-<div class="sideBar">
-        <h3>Featured Film</h3>
-        <div> <img src="images/babadook.jpg" alt="Babadook" /> </div>
-        <p>Info Here</p>
-      </div>
-    </section>
-  </main>
-</div>
-<?php
-// add Footer
-?>
-<script src="js/main.js"></script>
+
+      <section class="twoColumn">
+        <div>
+
+          <form action="thankyou.php" method="post">
+
+            <div>
+              <label for="firstname">First name</label>
+              <input type="text" name="firstname" id="firstname" placeholder="Your First Name"/>
+            </div>
+
+            <div>
+              <label for="surname">Surname</label>
+              <input type="text" name="surname" id="surname" placeholder="Your Surname"/>
+            </div>
+
+            <div>
+              <label for="Email">Email</label>
+              <input type="email" name="Email" id="Email" required />
+            </div>
+
+            <div>
+              <label for="Tel">Contact Telephone</label>
+              <input type="tel" name="Tel" id="Tel" required />
+            </div>
+
+            <div>
+              <label for="marketing">How did you hear about us?</label>
+              <select name="marketing" id="marketing">
+                <option value="Web">Web</option>
+                <option value="Friend">Friend</option>
+                <option value="Other">Other</option>
+              </select>
+            </div>
+
+            <div>
+              <input type="submit" value="Send" class="sendButton" />
+            </div>
+          </form>
+
+        </div>
+
+        <div class="sideBar">
+
+          <h3>Featured Film</h3>
+          <div> <img src="images/babadook.jpg" alt="Babadook" /> </div>
+          <p>Info Here</p>
+
+        </div>
+      </section>
+    </main>
+  </div>
+
+  <?php
+    // add Footer
+    include("includes/footer.php");
+  ?>
+
+  <script src="js/main.js"></script>
 </body>
 </html>
--- thankyou.php
+++ thankyou.php
@@ -1,39 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>Thank You</title>
-<link rel="stylesheet" href="css/mobile.css" />
-<link
-      rel="stylesheet"
-      href="css/desktop.css"
-      media="only screen and (min-width : 720px)"
-    />
-</head>
-<body>
-<?php
-include("includes/header.php");
-?>
-<div class="mainContainer">
-  <main>
-    <div class="banner">
-      <h2>Thank You</h2>
-    </div>
-    <section class="twoColumn">
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Thank You</title>
 
-<div class="sideBar">
-        <h3>Featured Film</h3>
-        <div> <img src="images/babadook.jpg" alt="Babadook" /> </div>
-        <p>Info Here</p>
+  <link rel="stylesheet" href="css/mobile.css" />
+  <link
+        rel="stylesheet"
+        href="css/desktop.css"
+        media="only screen and (min-width : 720px)"
+      />
+
+</head>
+
+<body>
+  <?php
+    include("includes/header.php");
+  ?>
+  <div class="mainContainer">
+    <main>
+      <div class="banner">
+        <h2>Thank You</h2>
       </div>
-    </section>
-  </main>
-</div>
-<?php
-// add Footer
-?>
-<script src="js/main.js"></script>
+
+      <section class="twoColumn">
+        <div> 
+          <table> 
+            <?php 
+              foreach($_POST as $key => $value){ 
+                echo "<tr>"; 
+                echo "<td class=\"uc\">"; 
+                echo $key; 
+                echo "</td>"; 
+                echo "<td>"; 
+                echo $value; 
+                echo "</td>"; 
+                echo "<tr>"; 
+              } 
+            ?> 
+          </table> 
+        </div> 
+
+        <div class="sideBar">
+          <h3>Featured Film</h3>
+          <div> <img src="images/babadook.jpg" alt="Babadook" /> </div>
+          <p>Info Here</p>
+        </div>
+
+      </section>
+    </main>
+  </div>
+
+  <?php
+    // add Footer
+  ?>
+  <script src="js/main.js"></script>
 </body>
 </html>
